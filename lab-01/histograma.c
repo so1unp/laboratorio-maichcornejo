6206d22cf07d1f358666ddbe56dd128b5fea85d8
mai <maichcornejo@gmail.com>
2024-03-16 20:21:17 -0300

    resolviendo lab 1

--- lab-01/histograma.c
+++ lab-01/histograma.c
@@ -1,36 +1,38 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#define max_long 30
+#define max_long 10
 
 int main(int argc, char *argv[]) {
-    char c;
-    int i, j;
-    int longitud = 1;
-    int contador[max_long+1] = {0};
+  char c;
+  int i, j;
+  int longitud = 0;
+  int contador[max_long + 1] = {0};
 
-    while ((c==getchar())!=EOF) {
-        if (c == ' ' || c == '\n'|| c=='\t') {
-            longitud;
-        } else {
-            longitud++;
-        }
-
-        while ((c != ' ')&& (c!=EOF)  && (c != '\n') && (c!= '\t')) {
-            longitud++;
-            c = getchar();
-        }
-
-        contador[longitud]++;
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n' || c == '\t') {
+	if(longitud>0 && longitud <= max_long){
+      	  contador[longitud]++;
+	}
+	longitud=0;
+    } else {
+      longitud++;
+      if (longitud> max_long){
+	longitud=0;	
+      while ((c==getchar())!= ' ' && c!= '\n' && c!='\t' && c!=EOF);
+      }
     }
-    //Imprimir histograma
+  }
 
-    for (i=1; i<max_long;i++){
-        printf("%d ", i);
-        for (j=0; j<contador [i]; j++){
-            printf("*");
-        }
-        printf("\n");
+  // Imprimir histograma verticalmente
+  for (i = 1; i <= max_long; i++) {
+    printf("%2d | ", i);
+    for (j = 0; j < contador[i]; j++) {
+      printf("*");
     }
-    // Termina la ejecución del programa.
-    exit(EXIT_SUCCESS);
+    printf("\n");
+  }
+
+  // Termina la ejecución del programa.
+  exit(EXIT_SUCCESS);
 }
